c768f6eb73097199f1098d05415dbe8a93de74bc
Jeswin Kumar <jeswinpk@agilehead.com>
2017-11-23 12:29:29 +0530

    WIP

--- src/bashfury.js
+++ src/bashfury.js
@@ -19,37 +19,33 @@ async function parse(args, input, option) {
       : option === "e"
         ? 1
         : isFilename(args[0])
-          ? (() => {
-            const evalText = isFilename ? `require(${args[0]}` : 
-            const res = eval(`require(${args[0]}`);
-            return typeof res === "function"
-              ? parse(args.slice(1), await res(input))
-              : parse(args.slice(1), res)
-          })()
-          : 
-      
-      (() => {
-          const cmd = args[0];
-          return isShellCommand ? 
-            
-          (() => {
-            
-            const result = eval(cmd);
-          })() : (() => {})();
-        })()
+          ? await (async () => {
+              const evalText = isFilename ? `require(${args[0]}` : 1;
+              const res = eval(`require(${args[0]}`);
+              return typeof res === "function"
+                ? parse(args.slice(1), await res(input))
+                : parse(args.slice(1), res);
+            })()
+          : (() => {
+              const cmd = args[0];
+              return isShellCommand
+                ? (() => {
+                    const result = eval(cmd);
+                  })()
+                : (() => {})();
+            })()
     : 1;
   return parse();
 }
 
 parse(process.argv);
 
-// each array item is [org, project, isActive][
-  export default [
-    ["jeswin", "bitfury", true],
-    ["jeswin", "lazily", true],
-    ["jeswin", "lazily-async", false],
-     ["bigyak","wild-yak", true],
-   ["bigyak","paddock", true],
-   ["bigyak","yakety-yak", false]
- ]
-
+// each array item is [org, project, isActive]
+export default [
+  ["jeswin", "bitfury", true],
+  ["jeswin", "lazily", true],
+  ["jeswin", "lazily-async", false],
+  ["bigyak", "wild-yak", true],
+  ["bigyak", "paddock", true],
+  ["bigyak", "yakety-yak", false]
+];
