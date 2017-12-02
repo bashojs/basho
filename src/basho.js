2fde3daac8b6fe31e9c40c93163d4cb38b09ba93
jeswin <jeswinpk@agilehead.com>
2017-12-02 17:13:58 +0530

    Fix version

--- src/basho.js
+++ src/basho.js
@@ -8,12 +8,16 @@ import haikus from "./haikus";
 
 if (process.argv.length > 2) {
   if (process.argv[2] === "-v" || process.argv[2] === "--version") {
-    console.log("0.0.7");
+    const packageJSON = require("../package.json");
+    console.log(packageJSON.version);
     process.exit(0);
   } else {
     getStdin()
       .then(async str => {
-        const input = str.replace(/\n$/, "").split("\n").filter(x => x !== "");
+        const input = str
+          .replace(/\n$/, "")
+          .split("\n")
+          .filter(x => x !== "");
         const output = await parse(
           input.concat(process.argv.slice(2)),
           undefined,
