ddf80ffcd267d9d8f67644a963a9071bf540780d
Jeswin Kumar <jeswinpk@agilehead.com>
2021-08-23 09:19:51 +0530

    Add yaml support

--- src/test/test.ts
+++ src/test/test.ts
@@ -206,6 +206,28 @@ describe("basho", () => {
     output.should.equal("1\n");
   });
 
+  it(`Treats the input as yaml`, async () => {
+    const echoProcess = child_process.spawn("echo", [
+      "-e",
+      '---\na:\n  b: hello\n',
+    ]);
+
+    const bashoProcess = child_process.spawn("node", [
+      script,
+      "--yaml",
+      "-j",
+      "x.a.b",
+    ]);
+
+    echoProcess.stdout.pipe(bashoProcess.stdin);
+
+    const output = await new Promise<string>((resolve) =>
+      bashoProcess.stdout.on("data", (x) => resolve(x.toString()))
+    );
+
+    output.should.equal("hello\n");
+  });
+
   it(`Filters an array`, async () => {
     const output = await execute(
       `${basho} [1,2,3,4] -f 'x\>2' -e 'echo \${x}'`
